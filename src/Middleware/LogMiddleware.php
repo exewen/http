a6437ca801062c3f8ae249c9859cdbbae0b1f29b
exeweb <exeweb@163.com>
2025-03-21 16:58:19 +0800

    commit_message

--- src/Middleware/LogMiddleware.php
+++ src/Middleware/LogMiddleware.php
@@ -4,6 +4,7 @@ namespace Exewen\Http\Middleware;
 
 use Exewen\Di\Container;
 use Exewen\Di\Context\ApplicationContext;
+use Exewen\Facades\AppFacade;
 use Exewen\Logger\Contract\LoggerInterface;
 use Exewen\Logger\Logger;
 use Psr\Http\Message\RequestInterface;
@@ -19,7 +20,7 @@ class LogMiddleware
 
     public function __construct(string $channel, array $config)
     {
-        $this->logger = ApplicationContext::getContainer()->get(LoggerInterface::class);
+        $this->logger = AppFacade::getContainer()->get(LoggerInterface::class);
     }
 
     public function __invoke(callable $handler): callable
